7dc3e7c3618ae50e571f94ed4d67b4fba8196a7a
Rishad <armreshad@gmail.com>
2024-12-23 23:31:46 +0300

    api updated

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { useEffect } from 'react';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,30 +7,10 @@ class MyDocument extends Document {
   }
 
   render() {
-    let isMetrics = false;
-    useEffect(() => {
-      setTimeout(() => {
-        isMetrics = true;
-      }, 15000);
-    }, []);
-
     return (
       <Html>
         <Head>
           <link rel="shortcut icon" href="/favicon.svg" />
-
-          <link
-            rel="preload"
-            href="/fonts/tt-ricordi-marmo-trial-variable.woff"
-            as="font"
-            type="font/woff"
-            crossOrigin="anonymous"
-          />
-          {/* microsoft bing verification */}
-          <meta
-            name="msvalidate.01"
-            content="C34B962949690979DF0ADC0147270090"
-          />
         </Head>
         <body>
           <Main />
