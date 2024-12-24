ea716bdf916bf8e54a470ab9b74130f3ad438484
Rishad <armreshad@gmail.com>
2024-12-24 23:47:26 +0300

    add playsInline to video component

--- components/store/homePage/Scanner.tsx
+++ components/store/homePage/Scanner.tsx
@@ -608,12 +608,14 @@ const Scanner = () => {
           objectFit: 'contain',
         }}
         autoPlay
+        muted
+        playsInline
       />
       <canvas
         ref={canvasRef}
         width="100%"
         height="100%"
-        style={{ position: 'absolute', top: 0, left: 0 }}
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%' }}
       />
       {scanResult && (
         <div>
