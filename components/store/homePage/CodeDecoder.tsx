f6dc3c64d31d436d60860866a86ffd3a79ea9854
ARM <armreshad@gmail.com>
2024-12-25 15:44:42 +0300

    new technic

--- components/store/homePage/CodeDecoder.tsx
+++ components/store/homePage/CodeDecoder.tsx
@@ -8,14 +8,12 @@ import styles from './styles/main.module.css';
 const BarcodeScannerComponent = dynamic(
   () => import('react-qr-barcode-scanner'),
   {
-    ssr: false, // Disable server-side rendering
+    ssr: false,
   },
 );
-// import { Scanner } from '@yudiel/react-qr-scanner';
-// const Scanner = dynamic(() =>
-//   import('@yudiel/react-qr-scanner').then((mod) => mod.Scanner),
-// );
-import Scanner from './Scanner';
+const Scanner = dynamic(() =>
+  import('@yudiel/react-qr-scanner').then((mod) => mod.Scanner),
+);
 
 const CodeDecoder = () => {
   const dispatch = useAppDispatch();
@@ -25,7 +23,7 @@ const CodeDecoder = () => {
 
   const [cameraOpenQr, setCameraOpenQr] = useState(false);
   const [cameraOpenBar, setCameraOpenBar] = useState(false);
-
+  const [isQrDetected, setIsQrDetected] = useState(false);
   const handleScanQrButtonClick = () => {
     if (cameraOpenBar) {
       openErrorNotification('Сначала закройте камеру для штрих-кода.');
@@ -53,6 +51,10 @@ const CodeDecoder = () => {
     }
   };
 
+  const handleError = (error) => {
+    console.error(error);
+  };
+
   return (
     <div className="App">
       <h1>Сканер QR-кода и штрих-кода</h1>
@@ -67,7 +69,7 @@ const CodeDecoder = () => {
         </button>
       </div>
 
-      <div className={styles.cameraWrapper} id="camera-container">
+      <div className={styles.cameraWrapper}>
         {cameraOpenQr && (
           // <BarcodeScannerComponent
           //   id="camera-view"
@@ -83,14 +85,23 @@ const CodeDecoder = () => {
           //   }}
           // />
           <Scanner
-          // onScan={(result) => {
-          //   if (result) {
-          //     setQrData(result[0].rawValue);
-          //     setCameraOpenQr(false);
-          //   } else {
-          //     setQrData('-');
-          //   }
-          // }}
+            onScan={(result) => {
+              console.log(result);
+
+              setIsQrDetected(true);
+              if (result) {
+                setQrData(result[0].rawValue);
+                setCameraOpenQr(false);
+              } else {
+                setQrData('-');
+              }
+            }}
+            onError={handleError}
+            constraints={{
+              facingMode: 'environment', // Use the rear camera
+              width: { min: 76, max: 380 }, // Adjust min/max width based on desired QR code size
+              height: { min: 76, max: 380 }, // Adjust min/max height based on desired QR code size
+            }}
           />
         )}
         {cameraOpenBar && (
